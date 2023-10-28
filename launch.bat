c65783634198d5586fa95d5efcc86b8271b38958
Luke <reeingspam@gmail.com>
2023-10-28 12:58:28 -0700

    skyraid purge

--- launch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting server...
-java -Xms4G -Xmx4G --add-modules=jdk.incubator.vector -jar pufferfish.jar nogui
-echo Server has shut down.
---
+++ start.cmd
@@ -0,0 +1,3 @@
+@echo off
+java -Xms4G -Xmx4G --add-modules=jdk.incubator.vector -jar server.jar nogui
+pause
